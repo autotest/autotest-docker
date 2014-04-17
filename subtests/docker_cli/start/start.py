e8da1df732458171a60823db52cfa6e37d80b641
Chris Evich <cevich@redhat.com>
2014-04-17 11:31:04 -0400

    Merge branch 'start3' of https://github.com/jzupka/autotest-docker into integration
    Signed-off-by: Chris Evich <cevich@redhat.com>
    
    Conflicts:
    	config_defaults/subtests/docker_cli/start.ini
    	subtests/docker_cli/start/start.py

--- subtests/docker_cli/start/start.py
+++ subtests/docker_cli/start/start.py
@@ -1,115 +1,172 @@
 """
-Test usage of docker 'start' command
+Test output of docker start command
 
-initialize:
-1) prepare docker container, but don't start it
-run_once:
-2) execute docker start using the nonexisting name
-3) execute the new container using `docker run`
-4) execute docker start using the running docker name
-5) stop the container
-6) execute docker start using the stopped docker name
-postprocess:
-7) analyze results
+1. Create new container.
+2. Wait till end of container command.
+3. Try to start container.
+4. Check if container was started.
 """
+
+import time
+import random
+from autotest.client.shared import error
+from dockertest.subtest import SubSubtest
+from dockertest.output import OutputGood
+from dockertest.dockercmd import AsyncDockerCmd, DockerCmd
+from dockertest.containers import DockerContainersCLI
+from dockertest.images import DockerImage
+from dockertest import subtest
+from dockertest import config
+
 # Okay to be less-strict for these cautions/warnings in subtests
 # pylint: disable=C0103,C0111,R0904,C0103
 
-import os
-import time
+class DockerContainersCLIWithOutSize(DockerContainersCLI):
 
-from dockertest import config, subtest, xceptions
-from dockertest.containers import DockerContainers
-from dockertest.dockercmd import (AsyncDockerCmd, NoFailDockerCmd,
-                                  MustFailDockerCmd)
-from dockertest.images import DockerImage
-from dockertest.output import OutputGood
-from dockertest.xceptions import (DockerCommandError, DockerExecError)
-from autotest.client.shared import utils
+    """
+    DockerContainersCLIWithOutSize remove size checking from
+    DockerContainersCLI for cmd time reduction.
+    """
+
+    #: This is probably test-subject related, be a bit more noisy
+    verbose = True
+
+        # private methods don't need docstrings
+    def _get_container_list(self):  # pylint: disable=C0111
+        return self.docker_cmd("ps -a --no-trunc",
+                               self.timeout)
 
 
-class start(subtest.Subtest):
+class DockerContainersCLIRunOnly(DockerContainersCLIWithOutSize):
 
-    """ Subtest caller """
+    """
+    DockerContainersCLI remove size checking from DockerContainersCLI.
+    It takes lots of time. Only running containers.
+    """
+
+    #: This is probably test-subject related, be a bit more noisy
+    verbose = True
+
+        # private methods don't need docstrings
+    def _get_container_list(self):  # pylint: disable=C0111
+        return self.docker_cmd("ps --no-trunc",
+                               self.timeout)
+
+
+class start(subtest.SubSubtestCaller):
     config_section = 'docker_cli/start'
 
-    def _init_stuff(self):
-        """ Initialize stuff """
-        self.stuff['container_name'] = None     # name of the container
+
+class start_base(SubSubtest):
 
     def initialize(self):
-        super(start, self).initialize()
-        self._init_stuff()
+        super(start_base, self).initialize()
         config.none_if_empty(self.config)
-        # Get free name
-        prefix = self.config["container_name_prefix"]
-        docker_containers = DockerContainers(self)
-        name = docker_containers.get_unique_name(prefix, length=4)
-        self.stuff['container_name'] = name
+        dc = DockerContainersCLIWithOutSize(self.parent_subtest)
+        self.sub_stuff["conts_obj"] = dc
+        dc = DockerContainersCLIRunOnly(self.parent_subtest)
+        self.sub_stuff["con_ro_obj"] = dc
 
-    def _start_container(self, name):
-        """ Create, store in self.stuff and execute container """
-        self.stuff['container_name'] = name
-        if self.config.get('run_options_csv'):
-            subargs = [arg for arg in
-                       self.config['run_options_csv'].split(',')]
-        else:
-            subargs = []
-        subargs.append("--name %s" % name)
-        fin = DockerImage.full_name_from_defaults(self.config)
-        subargs.append(fin)
-        subargs.append("bash")
-        subargs.append("-c")
-        subargs.append("'echo STARTED: $(date); while :; do sleep 0.1; done'")
-        container = AsyncDockerCmd(self, 'run', subargs)
-        container.execute()
-        utils.wait_for(lambda: container.stdout.startswith("STARTED"), 5,
-                       step=0.1)
+        self.sub_stuff["image_name"] = None
+        self.sub_stuff["container"] = None
+        self.sub_stuff["containers"] = []
 
     def run_once(self):
-        # Execute the start command
-        super(start, self).run_once()
-        name = self.stuff['container_name']
-        err_msg = ("Start of the %s container failed, but '%s' message is not "
-                   "in the output:\n%s")
-        # Nonexisting container
-        result = MustFailDockerCmd(self, "start", [name]).execute()
-        self.failif("No such container" not in str(result), err_msg
-                    % ("non-existing", 'No such container', result))
+        super(start_base, self).run_once()
+        # 1. Run with no options
+        dkrcmd = AsyncDockerCmd(self.parent_subtest, 'start',
+                                self.complete_docker_command_line(),
+                                self.config['docker_start_timeout'])
+        self.loginfo("Executing background command: %s" % dkrcmd)
+        dkrcmd.execute()
+        while not dkrcmd.done:
+            self.loginfo("Starting container...")
+            time.sleep(3)
+        self.sub_stuff["cmdresult"] = dkrcmd.wait()
 
-        # Running container
-        self._start_container(name)
-        result = MustFailDockerCmd(self, "start", [name]).execute()
-        self.failif("is already running" not in str(result), err_msg
-                    % ("running", "is already running", result))
+    def complete_docker_command_line(self):
+        cmds = []
 
-        # Stopped container
-        NoFailDockerCmd(self, "kill", [name]).execute()
-        result = NoFailDockerCmd(self, "start", [name]).execute()
+        if (self.config["docker_attach"]):
+            cmds.append("-a")
+        if (self.config["docker_interactive"]):
+            cmds.append("-i")
+
+        cmds.append(self.sub_stuff["container"].long_id)
+
+        self.sub_stuff["start_cmd"] = cmds
+
+        return cmds
 
     def postprocess(self):
-        super(start, self).postprocess()
-        name = self.stuff['container_name']
-        logs = AsyncDockerCmd(self, "logs", ['-f', name])
-        logs.execute()
-        utils.wait_for(lambda: logs.stdout.count("\n") == 2, 5, step=0.1)
-        out = logs.stdout
-        self.failif(out.count("\n") != 2, "The container was executed twice, "
-                    "there should be 2 lines with start dates, but is "
-                    "%s.\nContainer output:\n%s" % (out.count("\n"), out))
-        NoFailDockerCmd(self, "kill", [name]).execute()
+        super(start_base, self).postprocess()
+        # Raise exception if problems found
+        OutputGood(self.sub_stuff['cmdresult'])
+        if self.config["docker_expected_result"] == "PASS":
+            self.failif(self.sub_stuff['cmdresult'].exit_status != 0,
+                        "Non-zero start exit status: %s"
+                        % self.sub_stuff['cmdresult'])
+
+        elif self.config["docker_expected_result"] == "FAIL":
+            self.failif(self.sub_stuff['cmdresult'].exit_status == 0,
+                        "Zero start exit status: Command should fail due to"
+                        " wrong command arguments.")
 
     def cleanup(self):
-        super(start, self).cleanup()
-        cleanup_log = []
-        name = self.stuff.get('container_name')
-        if name and self.config.get('remove_after_test'):
-            try:
-                NoFailDockerCmd(self, 'rm', ['--force', '--volumes',
-                                             name]).execute()
-            except (DockerCommandError, DockerExecError), details:
-                cleanup_log.append("docker rm failed: %s" % details)
-        if cleanup_log:
-            msg = "Cleanup failed:\n%s" % "\n".join(cleanup_log)
-            self.logerror(msg)  # message is not logged nicely in exc
-            raise xceptions.DockerTestError(msg)
+        super(start_base, self).cleanup()
+        # Auto-converts "yes/no" to a boolean
+
+        if (self.config['remove_after_test'] and
+                'containers' in self.sub_stuff):
+            for cont in self.sub_stuff["containers"]:
+                try:
+                    cid = cont.long_id
+                    self.sub_stuff["conts_obj"].kill_container_by_long_id(cid)
+                    self.wait_for_container_death(cont)
+                except Exception, e:
+                    print e
+                    pass
+                self.sub_stuff["conts_obj"].remove_by_obj(cont)
+
+    def wait_for_container_death(self, container_obj):
+        cont_id = container_obj.long_id
+        prep_changes = DockerCmd(self.parent_subtest, "wait",
+                                 [cont_id],
+                                 self.config['docker_run_timeout'])
+
+        results = prep_changes.execute()
+        return results.exit_status
+
+
+class short_term_app(start_base):
+    config_section = 'docker_cli/start/short_term_app'
+
+    def initialize(self):
+        super(short_term_app, self).initialize()
+
+        docker_name = DockerImage.full_name_from_component(
+            self.config["docker_repo_name"],
+            self.config["docker_repo_tag"])
+        # Private to this instance, outside of __init__
+        prep_changes = DockerCmd(self.parent_subtest, "run",
+                                 ["-d",
+                                  docker_name,
+                                  self.config["run_cmd"]],
+                                 self.config['docker_run_timeout'])
+
+        results = prep_changes.execute()
+        if results.exit_status:
+            raise error.TestNAError("Problems during initialization of"
+                                    " test: %s" % results)
+        else:
+            cont_id = results.stdout.strip()
+            cont = self.sub_stuff["conts_obj"].list_containers_with_cid(cont_id)
+            if cont == []:
+                raise error.TestNAError("Problems during initialization of"
+                                        " test: Failed to find container with"
+                                        "id %s." % cont_id)
+            self.sub_stuff["container"] = cont[0]
+            self.sub_stuff["containers"].append(self.sub_stuff["container"])
+
+        if self.config["check_if_cmd_finished"]:
+            self.wait_for_container_death(cont[0])
