17618dbf1e132a22c7e1ea952f192aa33b40f451
Chris Evich <cevich@redhat.com>
2014-04-24 12:28:51 -0400

    subtests...export_import: Generalize & Simplify
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/import_export/import_export.py
+++ subtests/docker_cli/import_export/import_export.py
@@ -2,14 +2,14 @@
 Test output of docker import/export command
 """
 
-from autotest.client import utils
-from autotest.client.shared import error
 from dockertest.subtest import SubSubtest
 from dockertest.containers import DockerContainers
 from dockertest.images import DockerImages
 from dockertest.images import DockerImage
 from dockertest.output import OutputGood
 from dockertest.dockercmd import DockerCmd
+from dockertest.dockercmd import NoFailDockerCmd
+from dockertest import xceptions
 from dockertest import subtest
 
 # Okay to be less-strict for these cautions/warnings in subtests
@@ -21,6 +21,33 @@ class import_export(subtest.SubSubtestCaller):
 
 
 class import_export_base(SubSubtest):
+
+    def init_test_container(self, name):
+        self.sub_stuff["subargs"].insert(0, "--name=\"%s\"" % name)
+        dkrcmd = DockerCmd(self.parent_subtest, 'run',
+                           self.sub_stuff['subargs'],
+                           timeout=self.config['docker_timeout'])
+        dkrcmd.verbose = True
+        cmdresult = dkrcmd.execute()
+        if cmdresult.exit_status != 0:
+            xceptions.DockerTestNAError("Unable to prepare env for test: %s" %
+                                       (cmdresult))
+        cid = self.sub_stuff["cont"].list_containers_with_name(name)
+        self.failif(cid == [],
+                    "Unable to search container with name %s: details :%s" %
+                   (name, cmdresult))
+
+    def init_ei_dockercmd(self, export_arg_csv, import_arg_csv):
+        # Never execute()'d, just used for command property
+        import_dkrcmd = DockerCmd(self.parent_subtest,
+                                  "import", import_arg_csv.split(','))
+        # Actually executed command
+        export_args = export_arg_csv.split(',')
+        export_args.append(import_dkrcmd.command)
+        export_import_dkrcmd = NoFailDockerCmd(self.parent_subtest,
+                                               "export", export_args)
+        export_import_dkrcmd.verbose = True
+        self.sub_stuff['export_import_dkrcmd'] = export_import_dkrcmd
 
     def initialize(self):
         super(import_export_base, self).initialize()
@@ -52,62 +79,42 @@ class import_export_base(SubSubtest):
                     di.remove_image_by_full_name(image)
                     self.logdebug("Successfully removed test image: %s",
                                   image)
-                except error.CmdError, e:
+                except xceptions.DockerCommandError, e:
                     error_text = "tagged in multiple repositories"
                     if not error_text in e.result_obj.stderr:
                         raise
-
+                except xceptions.DockerTestError:
+                    pass  # best effort removal, maybe image wasn't there
 
 class simple(import_export_base):
 
     def initialize(self):
         super(simple, self).initialize()
-        rand_name = utils.generate_random_string(8).lower()
-        self.sub_stuff["rand_name"] = rand_name
-        self.sub_stuff["subargs"].insert(0, "--name=\"%s\"" % rand_name)
-
-        dkrcmd = DockerCmd(self.parent_subtest, 'run',
-                           self.sub_stuff['subargs'],
-                           timeout=self.config['docker_timeout'])
-        dkrcmd.verbose = True
-
-        # Runs in background
-        cmdresult = dkrcmd.execute()
-        if cmdresult.exit_status != 0:
-            error.TestNAError("Unable to prepare env for test: %s" %
-                              (cmdresult))
-
-        import_export_cmd = self.config['import_export_cmd']
-        self.sub_stuff['subargs_ei'] = (import_export_cmd %
-                                        {"container": rand_name})
-
-        c_name = self.sub_stuff["rand_name"]
+        # Test container setup
+        dc = DockerContainers(self.parent_subtest)
+        c_name = dc.get_unique_name(self.__class__.__name__)
         self.sub_stuff["containers"].append(c_name)
-        self.sub_stuff["images"].append(c_name)
-        cid = self.sub_stuff["cont"].list_containers_with_name(c_name)
-
-        self.failif(cid == [],
-                    "Unable to search container with name %s: details :%s" %
-                   (c_name, cmdresult))
+        self.init_test_container(c_name)
+        # export/import command setup
+        di = DockerImages(self.parent_subtest)
+        i_name = di.get_unique_name(c_name)  # easier debugging
+        self.sub_stuff["images"].append(i_name)
+        subdct = {"container": c_name, "image": i_name}
+        export_cmd_args = self.config['export_cmd_args'].strip() % subdct
+        import_cmd_args = self.config['import_cmd_args'].strip() % subdct
+        self.init_ei_dockercmd(export_cmd_args, import_cmd_args)
 
     def run_once(self):
-        self.loginfo("Starting background docker command, timeout %s seconds",
-                     self.config['docker_timeout'])
-
-        dkrcmd = DockerCmd(self.parent_subtest, 'export',
-                           [self.sub_stuff['subargs_ei']],
-                           timeout=self.config['docker_timeout'])
-        dkrcmd.verbose = True
-        # Runs in background
-        self.sub_stuff['cmdresult_import_export'] = dkrcmd.execute()
+        super(simple, self).run_once()
+        export_import_dkrcmd = self.sub_stuff['export_import_dkrcmd']
+        self.loginfo("Starting %s", export_import_dkrcmd)
+        self.sub_stuff['cmdresult'] = export_import_dkrcmd.execute()
 
     def postprocess(self):
-        super(import_export_base, self).postprocess()  # Prints out basic info
+        super(simple, self).postprocess()  # Prints out basic info
         # Fail test if bad command or other stdout/stderr problems detected
-
-        OutputGood(self.sub_stuff['cmdresult_import_export'])
-
-        str_import_export = self.sub_stuff['cmdresult_import_export']
-        self.failif(str_import_export.exit_status != 0,
+        cmdresult = self.sub_stuff['cmdresult']
+        OutputGood(cmdresult)
+        self.failif(cmdresult.exit_status != 0,
                     "Problem with export import cmd detail :%s" %
-                    str_import_export)
+                    cmdresult)
