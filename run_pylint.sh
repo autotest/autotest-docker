1da921b255a027bae13b1a911c06c6e1d43cf167
Chris Evich <cevich@redhat.com>
2014-05-09 14:28:33 -0400

    run_pylint: Show filenames in output
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- run_pylint.sh
+++ run_pylint.sh
@@ -2,13 +2,10 @@
 
 MSGFMT='{msg_id}:{line:3d},{column}: {obj}: {msg}'
 DISABLEMSG="I0011,R0801,R0904,R0921,R0922"
-SUBTESTDISABLEMSG="I0011,R0801,R0904,E1101,E1002,R0903,F0401,C0103,C0111,W0232"
 INIT_HOOK="
 AP = os.environ.get('AUTOTEST_PATH', '/usr/local/autotest')
 sys.path.append(os.path.abspath(AP + '/..'))
-print 'Injected into path %s' % sys.path[-1]
 sys.path.append(os.path.abspath('.'))
-print 'Injected into path %s' % sys.path[-1]
 import autotest
 import autotest.common
 "
@@ -17,37 +14,49 @@ import autotest.common
 MYDIR=$(dirname "$0")
 if [ "$PWD" != "$MYDIR" ]
 then
-    echo "Switching to top-level directory \'$MYDIR\'"
     cd "$MYDIR"
 fi
 
+trap "exit" INT
+
 echo -e "\n\n======================================= dockertest"
-find dockertest -name '*.py' -a -not -name '*_unittest*.py' | xargs \
+find dockertest -name '*.py' -a -not -name '*_unittest*.py' | \
+while read LINE; do
+    echo -e "Dockertest module: ${LINE} "
     pylint -rn --init-hook="$INIT_HOOK" \
            --disable="$DISABLEMSG" \
            --max-args=6 \
            --no-docstring-rgx='(__.*__)|(_.*)|(__init__)' \
            --output-format="colorized" \
            --rcfile=/dev/null \
-           --msg-template="$MSGFMT"
-
+           --msg-template="$MSGFMT" "${LINE}"
+    if [ "$?" -ne "0" ]
+    then
+        echo
+    fi
+done
 
 echo -e "\n\n======================================= subtests"
+SUBTESTDISABLEMSG="I0011,R0801,R0904,E1101,E1002,R0903,F0401,C0103,C0111,W0232,W0142"
 INIT_HOOK="
 AP = os.environ.get('AUTOTEST_PATH', '/usr/local/autotest')
 sys.path.append(os.path.abspath(AP + '/..'))
-print 'Injected into path %s' % sys.path[-1]
 sys.path.append(os.path.abspath('.'))
-print 'Injected into path %s' % sys.path[-1]
 import autotest
 import autotest.common
 import dockertest
 "
-find subtests -name '*.py' -a -not -name '*_unittest*.py' | xargs \
+find subtests -name '*.py' | while read LINE; do
+    echo -e "Subtest module: ${LINE} "
     pylint -rn --init-hook="$INIT_HOOK" \
            --disable="$SUBTESTDISABLEMSG" \
            --max-args=8 \
            --max-locals=20 \
            --output-format="colorized" \
            --rcfile=/dev/null \
-           --msg-template="$MSGFMT"
+           --msg-template="$MSGFMT" "${LINE}"
+    if [ "$?" -ne "0" ]
+    then
+        echo
+    fi
+done
