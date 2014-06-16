0bea9a94bce50d779a3cdf59f6832a7ea610ef92
Chris Evich <cevich@redhat.com>
2014-06-16 10:20:56 -0400

    Dockertest Version 0.8.2 (NO API Changes)
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- dockertest/version.py
+++ dockertest/version.py
@@ -31,7 +31,7 @@ MINOR = 8
 
 #: API Revision number, as an integer (range 0-255).  Not significant!
 #: for version comparisons. e.g. ``0.0.1 == 0.0.2 != 0.2.2``
-REVIS = 1
+REVIS = 2
 
 #: String format representation for MAJOR, MINOR, and REVIS
 FMTSTRING = "%d.%d.%d"
