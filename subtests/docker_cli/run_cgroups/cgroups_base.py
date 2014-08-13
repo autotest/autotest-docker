38cd6fb7e5b3606be125e47c38cb7de0f409f136
Chris Evich <cevich@redhat.com>
2014-08-13 14:22:11 -0400

    subtests...run_memory -> run_cgroups
    *  Renamed configuration file name to match
    *  Reworked class hierarchy to make adding non-memory related subsubtests
       easier
    *  Split out memory and cpu tests into their own modules
    *  Moved some common code up into new base-class
    *  Moved specific subtest code down into specific base-classes
    *  Implemented simple subsubtest to check unspecified, zero, and
       non-zero values for docker run -c
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ subtests/docker_cli/run_cgroups/cgroups_base.py
@@ -0,0 +1,70 @@
+"""
+Base class for cgroups testing
+"""
+
+import os
+from dockertest import xceptions
+from dockertest.dockercmd import NoFailDockerCmd
+from dockertest.dockercmd import DockerCmd
+from dockertest.containers import DockerContainers
+from dockertest.subtest import SubSubtest
+
+
+class cgroups_base(SubSubtest):
+
+    @staticmethod
+    def cgroup_fullpath(long_id, path, content):
+        """
+        Return full cgroup path for a container.
+        :param long_id: a container's long id
+        :path: the cgroup path of container
+        :param content: the value need check.
+        """
+        return os.path.join("%s-%s.scope" % (path, long_id), content)
+
+    @staticmethod
+    def read_cgroup(long_id, path, content):
+        """
+        Read container's cgroup file, return its value
+        :param long_id: a container's long id, can get via command --inspect.
+        :param path: the cgroup path of container.
+        :param content: the value need read.
+        """
+        cgroup_path = cgroups_base.cgroup_fullpath(long_id, path, content)
+        if not os.path.exists(cgroup_path):
+            raise xceptions.DockerTestNAError("Docker cgroup path "
+                                              "doesn't exist: %s"
+                                              % cgroup_path)
+        cgroup_file = open(cgroup_path, 'r')
+        try:
+            cgroup_value = cgroup_file.read()
+        finally:
+            cgroup_file.close()
+        return cgroup_value
+
+    def container_json(self, name, content):
+        """
+        Return container's json value.
+        :param name: An existing container's name
+        :param content: What the json value need get
+        """
+        inspect_id_args = ['--format={{.%s}}' % content]
+        inspect_id_args.append(name)
+        container_json = NoFailDockerCmd(self,
+                                         'inspect',
+                                         inspect_id_args)
+        content_value = container_json.execute().stdout.strip()
+
+        return content_value
+
+    def initialize(self):
+        super(cgroups_base, self).initialize()
+        self.sub_stuff['docker_containers'] = DockerContainers(self)
+        self.sub_stuff['name'] = []
+
+    def cleanup(self):
+        super(cgroups_base, self).cleanup()
+        if self.config['remove_after_test']:
+            for name in self.sub_stuff.get('name', []):
+                DockerCmd(self, 'kill', [name]).execute()
+                DockerCmd(self, 'rm', ['--force', name]).execute()
---
+++ subtests/docker_cli/run_cgroups/cpushares.py
@@ -0,0 +1,100 @@
+"""
+Simple tests that check output/behavior of ``docker run`` wuth ``-c``
+parameter.  It verifies that the container's cgroup resources
+match value passed and if the container can handle invalid values
+properly.
+"""
+
+from dockertest import xceptions
+from dockertest.dockercmd import NoFailDockerCmd
+from dockertest.images import DockerImage
+from cgroups_base import cgroups_base
+
+
+class cpu_base(cgroups_base):
+
+    @staticmethod
+    def check_cpushares(docker_cpushares, cgroup_cpushares, json_cpushares):
+        """
+        Compare container's cpu shares set by option -c, and its cgroup
+        cpu.shares
+
+        :param docker_cpushares: Value supplied to docker
+        :param cgroup_cpushares: Value reported in cgroup file
+        :param json_cpushares: Value reported in docker inspect
+        :return: Dictionary containing PASS/FAIL key with details
+        """
+        docker_cpushares = int(docker_cpushares)
+        cgroup_cpushares = int(cgroup_cpushares)
+        json_cpushares = int(json_cpushares)
+        msg = ("Container cpu shares: %s, "
+               "cgroup cpu shares: %s, "
+               "inspect cpu shares %s"
+               % (docker_cpushares, cgroup_cpushares, json_cpushares))
+        # therefor cgroup_cpushares == json_cpushares
+        matches = [docker_cpushares == cgroup_cpushares,
+                   docker_cpushares == json_cpushares]
+        if all(matches):
+            return {'PASS': msg, 'FAIL': None}
+        else:
+            return {'FAIL': msg, 'PASS': None}
+
+    def initialize(self):
+        super(cpu_base, self).initialize()
+        dc = self.sub_stuff['docker_containers']
+        image = DockerImage.full_name_from_defaults(self.config)
+        cpushares_value = self.config.get('cpushares_value', None)
+        if cpushares_value is None:
+            subargs = []
+        else:
+            subargs = ['--cpu-shares=%s' % cpushares_value]
+        name_prefix = self.config['name_prefix']
+        name = self.sub_stuff['name'] = dc.get_unique_name(name_prefix)
+        subargs += ['--name=%s' % name,
+                    '--detach',
+                    '--tty',
+                    image,
+                    '/bin/bash']
+        self.sub_stuff['subargs'] = subargs
+        self.sub_stuff['result'] = []
+
+    def run_once(self):
+        super(cpu_base, self).run_once()
+        subargs = self.sub_stuff['subargs']
+        dc = self.sub_stuff['docker_containers']
+        NoFailDockerCmd(self, 'run', subargs).execute()
+        cobjs = dc.list_containers_with_name(self.sub_stuff['name'])
+        long_id = cobjs[0].long_id
+        json_cpushares = dc.json_by_long_id(long_id)[0]["Config"]["CpuShares"]
+        cgpath = self.config['cgroup_path']
+        cgvalue = self.config['cgroup_key_value']
+        cgroup_cpushares = self.read_cgroup(long_id, cgpath, cgvalue)
+        docker_cpushares = self.config.get('cpushares_value', 0)
+        self.sub_stuff['result'] = self.check_cpushares(docker_cpushares,
+                                                        cgroup_cpushares,
+                                                        json_cpushares)
+
+    def postprocess(self):
+        super(cpu_base, self).postprocess()
+        result = self.sub_stuff['result']
+        invalid = xceptions.DockerTestError("Invalid result %s" % result)
+        if self.config['expect_success'] == "PASS":
+            if result['FAIL'] is not None:
+                raise xceptions.DockerTestFail(result)
+            elif result['PASS'] is not None:
+                self.logdebug(result)
+            else:
+                raise invalid
+        else:
+            self.loginfo("Command expected to fail!")
+            if result['PASS'] is not None:
+                raise xceptions.DockerTestFail(result)
+            elif result['FAIL'] is not None:
+                self.logdebug(result)
+            else:
+                raise invalid
+
+    def cleanup(self):
+        # Expects list of names to remove
+        self.sub_stuff['name'] = [self.sub_stuff['name']]
+        super(cpu_base, self).cleanup()
