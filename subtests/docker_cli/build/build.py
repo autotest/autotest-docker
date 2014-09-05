adce0cd6a06ae990dd927aab96e818d6817d97c3
Lukáš Doktor <ldoktor@redhat.com>
2014-09-05 10:04:49 +0200

    subtests.docker_cli: Add 3 variants with bad Dockerfile
    This extension covers `docker build` with `-q`, `--force-rm` and
    the behavior when the build fails.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- subtests/docker_cli/build/build.py
+++ subtests/docker_cli/build/build.py
@@ -103,6 +103,11 @@ class build_base(subtest.SubSubtest):
         if not path:
             raise DockerTestNAError("config['dockerfile_path'] not provided")
         build_def['dockerfile_path'] = self.dockerfile_path(path)
+        im_cnt = self.config.get('docker_build_intermediary_containers')
+        build_def['intermediary_containers'] = im_cnt
+        build_def['build_fail_msg'] = self.config.get('docker_build_fail_msg')
+        build_def['stdout'] = self.config.get('docker_build_stdout')
+        build_def['no_stdout'] = self.config.get('docker_build_no_stdout')
 
     def run_once(self):
         super(build_base, self).run_once()
@@ -111,6 +116,9 @@ class build_base(subtest.SubSubtest):
                               [self.config['docker_build_options']])
 
     def _build_container(self, build_def, subargs):
+        """
+        Build container according to the `build_def` dictionary.
+        """
         subargs += ["-t", build_def['image_name'],
                     build_def['dockerfile_path']]
         dkrcmd = DockerCmd(self, 'build', subargs,
@@ -118,46 +126,139 @@ class build_base(subtest.SubSubtest):
                            verbose=True)
         build_def['result'] = dkrcmd.execute()
 
-    def postprocess(self):
-        super(build_base, self).postprocess()
-        for build_def in self.sub_stuff['builds']:
-            self.postprocess_result(build_def)
+    def _postprocess_exit_status(self, build_def):
+        """
+        Check the exit status and eventually the build_fail_msg.
+        """
+        if build_def.get('build_fail_msg'):
+            self.failif(build_def['result'].exit_status == 0, "Build returned "
+                        "0 even thought it was expected to fail: %s"
+                        % build_def['result'])
+            out = "%s\n%s" % (build_def['result'].stdout,
+                              build_def['result'].stderr)
+            exp = build_def.get('build_fail_msg')
+            self.failif(exp not in out, "Expected failure message '%s' not "
+                        "found in the build output:\n%s"
+                        % (exp, build_def['result']))
+        else:
+            OutputGood(build_def['result'])
+            self.failif(build_def['result'].exit_status != 0, "Non-zero build "
+                        "exit status: %s" % build_def['result'])
+        self.logdebug("%s:\tExit status\tOK", build_def['image_name'])
 
-    def postprocess_result(self, build_def):
+    def _postprocess_created_images(self, build_def):
         """
-        Go through results and check all containers were created
+        Check that all expected images were created
         """
-        # Exit status
-        OutputGood(build_def['result'])
-        self.failif(build_def['result'].exit_status != 0, "Non-zero build "
-                    "exit status: %s" % build_def['result'])
-        # Named image
         dkrimgs = self.sub_stuff['di']
-        imgs = dkrimgs.list_imgs_with_full_name(build_def['image_name'])
-        self.failif(len(imgs) < 1, "Test image '%s' not found in images\n%s"
-                    % (build_def['image_name'], dkrimgs.list_imgs_full_name()))
+        # Named image
+        if not build_def.get('build_fail_msg'):     # Only when build passed
+            imgs = dkrimgs.list_imgs_with_full_name(build_def['image_name'])
+            self.failif(len(imgs) < 1, "Test image '%s' not found in images\n"
+                        "%s" % (build_def['image_name'],
+                                dkrimgs.list_imgs_full_name()))
         # Intermediary images
         dkrimgs.images_args += " -a"    # list all
         images = dkrimgs.list_imgs()
         dkrimgs.images_args = dkrimgs.images_args[:-3]
-        for img_id in RE_IMAGES.findall(build_def['result'].stdout):
+        created_images = RE_IMAGES.findall(build_def['result'].stdout)
+        for img_id in created_images:
             imgs = [_.long_id for _ in images if _.cmp_id(img_id)]
             self.failif(len(imgs) != 1, "Intermediary image '%s' not present "
                         "once in images\n%s" % (img_id, images))
+        self.logdebug("%s:\tMain image + %s intermediary images\tOK",
+                      build_def['image_name'], len(created_images))
+
+    def _postprocess_created_containers(self, build_def):
+        """
+        Check that used containers were (not) removed
+        """
         # Intermediary containers
         containers = self.sub_stuff['dc'].list_containers()
-        if build_def.get('intermediary_containers'):    # should be preserved
-            for cont in RE_CONTAINERS.findall(build_def['result'].stdout):
+        created_containers = RE_CONTAINERS.findall(build_def['result'].stdout)
+        if build_def.get('intermediary_containers') == 'LAST':
+            # Only last one should be present (use this when build fails)
+            for cont in created_containers[:-1]:     # All but one exist
+                conts = [_.long_id for _ in containers if _.cmp_id(cont)]
+                self.failif(len(conts) != 0, "Intermediary container '%s' is "
+                            "present although it should been removed by build"
+                            "\n%s" % (cont, containers))
+            # Last one should not
+            conts = [_.long_id for _ in containers
+                     if _.cmp_id(created_containers[-1])]
+            self.failif(len(conts) != 1, "Intermediary container '%s' not "
+                        "present once in containers\n%s"
+                        % (created_containers[-1], containers))
+        elif build_def.get('intermediary_containers'):    # should be preserved
+            for cont in created_containers:
                 conts = [_.long_id for _ in containers if _.cmp_id(cont)]
                 self.failif(len(conts) != 1, "Intermediary container '%s' not "
                             "present once in containers\n%s" % (cont,
                                                                 containers))
         else:   # should not be present
-            for cont in RE_CONTAINERS.findall(build_def['result'].stdout):
+            for cont in created_containers:
                 conts = [_.long_id for _ in containers if _.cmp_id(cont)]
                 self.failif(len(conts) != 0, "Intermediary container '%s' is "
                             "present although it should been removed by build"
                             "\n%s" % (cont, containers))
+        self.logdebug("%s:\t%s intermediary containers\tOK",
+                      build_def['image_name'], len(created_containers))
+
+    def _postprocess_output(self, build_def):
+        """
+        Checks the presence of messages in the output
+        """
+        result = build_def['result']
+        exp = build_def.get('stdout')
+        self.failif(exp and not re.search(exp, result.stdout, re.M), "Expected"
+                    " message '%s' not found in build stdout:\n%s"
+                    % (exp, result))
+        exp = build_def.get('no_stdout')
+        self.failif(exp and re.search(exp, result.stdout, re.M), "Forbidden "
+                    "message '%s' was found in build stdout:\n%s"
+                    % (exp, result))
+        self.logdebug("%s:\tOutput messages\tOK", build_def['image_name'])
+
+    def _postprocess_no_containers(self):
+        """
+        Check that during test run expected number of containers were created
+        """
+        containers_pre = self.sub_stuff['existing_containers']
+        containers_post = self.sub_stuff['dc'].list_container_ids()
+        _all = self.config.get('dockerfile_all_containers', 0)
+        _new = self.config.get('dockerfile_new_containers', 0)
+        diff = len(containers_post) - len(containers_pre)
+        if _new != 0:
+            # No new containers
+            self.failif(diff == 0, "No new containers created in build "
+                        "(rm=False).")
+        # Too many new containers
+        if _all != _new:
+            self.failif(diff == _all, "Number of containers before and after "
+                        "second build (--rm=False) is exactly of %s higher, "
+                        "cache was probably not used and containers for all "
+                        "(even cached) steps were created." % _all)
+        # Other count
+        self.failif(diff != _new, "Number of containers before and after "
+                    "second build (--rm=False) is not of %s containers higher."
+                    " That's really weird...).\nBefore: %s\nAfter: %s"
+                    % (_new, containers_pre, containers_post))
+        self.logdebug("ALL:\tNumber of created containers\tOK")
+
+    def _postprocess_result(self, build_def):
+        """
+        Go through results and check all containers were created
+        """
+        self._postprocess_exit_status(build_def)
+        self._postprocess_created_images(build_def)
+        self._postprocess_created_containers(build_def)
+        self._postprocess_output(build_def)
+
+    def postprocess(self):
+        super(build_base, self).postprocess()
+        for build_def in self.sub_stuff['builds']:
+            self._postprocess_result(build_def)
+        self._postprocess_no_containers()
 
     def cleanup(self):
         super(build_base, self).cleanup()
@@ -183,16 +284,31 @@ class local_path(build_base):
     """
     Path to a local directory within the Dockerfile and other files are present
     """
-    pass
 
 
 class https_file(build_base):
 
     """ https path to a Dockerfile """
-    pass
 
 
 class git_path(build_base):
 
     """ path to a git reporistory which contains Dokerfile and othe files """
-    pass
+
+
+class bad(build_base):
+
+    """
+    Dockerfile with incorrect command (using --rm - last container should
+    be preserved because of the failure
+    """
+
+
+class bad_quiet(build_base):
+
+    """ Dockerfile with incorrect command (using --quiet) """
+
+
+class bad_force_rm(build_base):
+
+    """ Dockerfile with incorrect command + using --force-rm """
--- subtests/docker_cli/build/rm_false.py
+++ subtests/docker_cli/build/rm_false.py
@@ -30,26 +30,3 @@ class rm_false(build_base):
                     % (containers_pre, containers_post))
         self._build_container(self.sub_stuff['builds'][1],
                               [self.config['docker_build_options2']])
-
-    def postprocess(self):
-        super(rm_false, self).postprocess()
-        containers_pre = self.sub_stuff['existing_containers']
-        containers_post = self.sub_stuff['dc'].list_container_ids()
-        # No new containers
-        self.failif(len(containers_pre) == len(containers_post), "No new "
-                    "containers created in build (rm=False).")
-        # Too many new containers
-        _all = self.config['dockerfile_all_containers']
-        _new = self.config['dockerfile_new_containers']
-        if _all != _new:
-            self.failif(len(containers_pre) + _all == len(containers_post),
-                        "Number of containers before and after second build "
-                        "(--rm=False) is exactly of %s higher, cache was "
-                        "probably not used and containers for all (even "
-                        "cached) steps were created." % _all)
-        # Other count
-        self.failif(len(containers_pre) + _new != len(containers_post),
-                    "Number of containers before and after second build "
-                    "(--rm=False) is not of %s containers higher. That's "
-                    "really weird...).\nBefore: %s\nAfter: %s"
-                    % (_new, containers_pre, containers_post))
