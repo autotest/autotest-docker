505f731e3712d9fe683fb2766737b00cc85539ed
Chris Evich <cevich@redhat.com>
2014-09-09 13:09:58 -0400

    subtests...run_env: Code re-organization
    *  In order to better support base-class re-use, moved
       client/server code strings (data) into attribues on
       ``run_env_base()`` base class.  This way sub-classes
       are free to modify the strings w/o affecting other
       subtests.
    
    *  In order to better support behavior re-use, moved
       ``check_env()`` and ``get_env()`` into base-class.
       Removed leading underscore from ``_init_container`` method.
       Moved contents of ``_cleanup_containers`` directly into
       ``cleanup()`` where it can be overridden if needed.
    
    *  Fixed bug in cleanup where first 'already removed' container
       would cause removal to stop instead of continuing.
    
    *  Fixed bug in port.run_once() where stdin could
       be closed closed to container out of sync. with
       it's execution state.
    
    *  Synchronization with server and client container
       code execution (sent via stdin) incorrectly
       depended on CID availability rather than state
       of executing code.  Modified code strings to emit
       unique strings, added ''utils.wait_for()'' calls
       to synchronize on them instead.
    
    *  Moved client/server container creation out of
       port.run_once() and into separate methods.  This
       will allow better code re-use if/when a subclass
       needs to override run_once()
    
    *  Provide more uniform access pattern to
       InteractiveAsyncDockerCmd() stdin by making
       it into a property (just like stdout & stderr).
       Move line-by-line feeding from execute() into
       stdin setter.  Add attribute options for
       feeding in data all at once or line-by-line
       (default).  Add attribute option for extra
       checking each line ends with ``\n`` character.
    
    *  Fixed bug where containers were removed even if
       remove_after_test was set False.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/run_env/run_env.py
+++ subtests/docker_cli/run_env/run_env.py
@@ -5,50 +5,14 @@ import ast
 import os
 import random
 import re
-import time
 
 from autotest.client import utils
 from dockertest import config, xceptions
 from dockertest.containers import DockerContainers
-from dockertest.dockercmd import DockerCmd, AsyncDockerCmd
+from dockertest.dockercmd import DockerCmd
+from dockertest.dockercmd import AsyncDockerCmd
 from dockertest.images import DockerImage
 from dockertest.subtest import SubSubtestCaller, SubSubtest
-
-
-# requires: container, port, data
-PYTHON_CLIENT = """import socket
-import os
-print "ENVIRON = " + str(os.environ)
-addr = os.environ['%(container)s_PORT_%(port)s_TCP_ADDR']
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((addr, %(port)s))
-sock.send("%(data)s")
-print sock.recv(1024)
-sock.close()
-
-exit()
-"""
-
-# requires: port
-PYTHON_SERVER = """import socket
-import os
-print "ENVIRON = " + str(os.environ)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("", %(port)s))
-sock.listen(True)
-conn, addr = sock.accept()
-print "Connected by", addr
-while True:
-    data = conn.recv(1024)
-    print "RECEIVED: " + data
-    if not data:
-        break
-    conn.send("RESENDING: " + data)
-
-conn.close()
-
-exit()
-"""
 
 
 # FIXME: Remove this when BZ1131592 is resolved
@@ -58,6 +22,9 @@ class InteractiveAsyncDockerCmd(AsyncDockerCmd):
     Execute docker command as asynchronous background process on ``execute()``
     with PIPE as stdin and allows use of stdin(data) to interact with process.
     """
+
+    stdin_by_lines = True
+    guarantee_newlines = True
 
     def __init__(self, subtest, subcmd, subargs=None, timeout=None,
                  verbose=True):
@@ -74,18 +41,33 @@ class InteractiveAsyncDockerCmd(AsyncDockerCmd):
         ps_stdin, self._stdin = os.pipe()
         ret = super(InteractiveAsyncDockerCmd, self).execute(ps_stdin)
         os.close(ps_stdin)
-        if stdin:
-            for line in stdin.splitlines(True):
-                self.stdin(line)
+        self.stdin = stdin
         return ret
 
+    @property
+    def stdin(self):
+        """
+        Return open file instance connected to processes stdin
+        """
+        return os.fdopen(self._stdin, 'w', 0)
+
+    @stdin.setter
     def stdin(self, data):
         """
         Sends data to stdin (partial send is possible!)
         :param data: Data to be send
         :return: Number of written data
         """
-        return os.write(self._stdin, data)
+        if data:
+            if self.stdin_by_lines:
+                for line in str(data).splitlines(True):  # Preserve '\n'
+                    if self.guarantee_newlines:
+                        # Make sure every (or only) line ends with '\n'
+                        if line[-1] != '\n':
+                            line += '\n'
+                    os.write(self._stdin, line)
+            else:
+                os.write(self._stdin, data)
 
     def close(self):
         """
@@ -105,17 +87,84 @@ class run_env_base(SubSubtest):
 
     """ Base class """
 
-    def _init_container(self, prefix, subargs, cmd):
+    # Note: requires: container, port, data
+    python_client = (
+        """import socket\n"""
+        """import os\n"""
+        """# run_env_base.get_env() depends on this behavior\n"""
+        """\n"""
+        """print "ENVIRON = " + str(os.environ)\n"""
+        """addr = os.environ['%(container)s_PORT_%(port)s_TCP_ADDR']\n"""
+        """sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)\n"""
+        """# port.run_once() depends on this output, not statement\n"""
+        """\n"""
+        """print "Client", "Connecting"\n"""
+        """sock.connect((addr, %(port)s))\n"""
+        """sock.send("%(data)s")\n"""
+        """print sock.recv(1024)\n"""
+        """sock.close()\n"""
+        """\n"""
+        """exit()\n"""
+    )
+
+    # Note: requires: port
+    python_server = (
+        """import socket\n"""
+        """import os\n"""
+        """# run_env_base.get_env() depends on this behavior\n"""
+        """\n"""
+        """print "ENVIRON = " + str(os.environ)\n"""
+        """sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)\n"""
+        """sock.bind(("", %(port)s))\n"""
+        """sock.listen(True)\n"""
+        """# port.run_once() depends on this output, not statement\n"""
+        """\n"""
+        """print "Server", "Listening"\n"""
+        """conn, addr = sock.accept()\n"""
+        """print "Connected by", addr\n"""
+        """data = conn.recv(1024)\n"""
+        """print "RECEIVED: " + data\n"""
+        """conn.send("RESENDING: " + data)\n"""
+        """\n"""
+        """conn.close()\n"""
+        """\n"""
+        """exit()\n"""
+    )
+
+    def get_env(self, output):
+        """
+        Check output for token, return value as if parsed in python
+        """
+        for line in output.splitlines():
+            if line.startswith('ENVIRON = {'):
+                return ast.literal_eval(line[10:])
+        self.failif(True, "Fail to get env from output\n%s" % output)
+
+    def check_env(self, exp, variable, env, container, appendix):
+        """
+        Check that variable's value matches the exp regexp. Fails the test
+        when it's not.
+        :param exp: Expected value (string)
+        :param variable: Name of the env variable (string)
+        :param env: Environment (dictionary)
+        :param container: Name of the container (string)
+        :param appendix: Rest of the error string
+        """
+        self.failif(not re.match(exp, env.get(variable)), "%s=%s not found in"
+                    " %s env:\n%s%s" % (variable, exp, container, env,
+                                        appendix))
+
+    def init_container(self, prefix, subargs, cmd):
         """
         Prepares dkrcmd and stores the name in self.sub_stuff['containers']
         :return: tuple(dkrcmd, name)
         """
         name = self.sub_stuff['dc'].get_unique_name(prefix, length=4)
         subargs.append("--name %s" % name)
+        self.logdebug("Queuing container %s for removal", name)
         self.sub_stuff['containers'].append(name)
         fin = DockerImage.full_name_from_defaults(self.config)
         subargs.append(fin)
-        subargs.append('bash -c')
         subargs.append(cmd)
         dkrcmd = InteractiveAsyncDockerCmd(self, 'run', subargs, verbose=False)
         return dkrcmd, name
@@ -127,24 +176,23 @@ class run_env_base(SubSubtest):
         self.sub_stuff['dc'] = DockerContainers(self.parent_subtest)
         self.sub_stuff['containers'] = []
 
-    def _cleanup_containers(self):
-        """
-        Cleanup the containers defined in self.sub_stuff['containers']
-        """
+    def cleanup(self):
+        super(run_env_base, self).cleanup()
+        if not self.config['remove_after_test']:
+            return
         for name in self.sub_stuff['containers']:
             conts = self.sub_stuff['dc'].list_containers_with_name(name)
             if conts == []:
-                return  # Docker was already removed
+                self.logdebug("Container %s was already removed",
+                              name)
+                continue  # Docker was already removed
             elif len(conts) > 1:
                 msg = ("Multiple containers match name '%s', not removing any"
                        " of them...", name)
                 raise xceptions.DockerTestError(msg)
+            self.logdebug("Removing %s", name)
             DockerCmd(self, 'rm', ['--force', '--volumes', name],
                       verbose=False).execute()
-
-    def cleanup(self):
-        super(run_env_base, self).cleanup()
-        self._cleanup_containers()
 
 
 class port(run_env_base):
@@ -166,88 +214,70 @@ class port(run_env_base):
                   'container': 'SERVER',
                   'data': ('Testing data %s'
                            % utils.generate_random_string(12))}
-        self.sub_stuff['client_params'] = params
+        self.sub_stuff['params'] = params
         self.sub_stuff['client_env'] = utils.generate_random_string(6)
 
-    def run_once(self):
-        super(port, self).run_once()
-        params = self.sub_stuff['client_params']
-        # start server
+    def start_server(self):
+        params = self.sub_stuff['params']
         subargs = ['-i', '-t', '-p %s' % params['port'],
                    '-e ENV_VARIABLE=%s' % self.sub_stuff['server_env']]
 
-        servercmd, server = self._init_container('server', subargs, 'python')
-        servercmd.execute(PYTHON_SERVER % params)
+        servercmd, server = self.init_container('server', subargs, 'python')
+        self.sub_stuff['server_name'] = server
+        servercmd.execute()
+        return servercmd
 
-        # FIXME: Need to use ID instead of name, because docker-autotest can't
-        # handle linked containers as the names are also listed, eg:
-        # server_cuP0 => client_qYjF/server,server_cuP0
-        for _ in xrange(100):
-            cont = self.sub_stuff['dc'].list_containers_with_name(server)
-            if len(cont) == 1:
-                serverid = cont[0].long_id
-                self.sub_stuff['containers'].append(serverid)
-                break
-            time.sleep(0.1)
-        else:
-            self.failif(True, "Container '%s' didn't started in 10s" % server)
-
-        # start client
-        subargs = ['-i', '-t', '--link %s:server' % server,
+    def start_client(self):
+        server_name = self.sub_stuff['server_name']
+        subargs = ['-i', '-t', '--link %s:server' % server_name,
                    '-e ENV_VARIABLE=%s' % self.sub_stuff['client_env']]
-        clientcmd, client = self._init_container('client', subargs, 'python')
+        clientcmd, client = self.init_container('client', subargs, 'python')
         self.sub_stuff['client_name'] = client
-        clientcmd.execute(PYTHON_CLIENT % params)
+        clientcmd.execute()
+        return clientcmd
 
-        # We can't wait for the process to finish, because currently it waits
-        # until stdin is closed :-(
-        for _ in xrange(100):
-            conts = self.sub_stuff['dc'].list_containers_with_cid(serverid)
-            if (len(conts) == 1 and conts[0].status
-                    and "Exited" in conts[0].status):
-                break   # Container finished, proceed to results
-            time.sleep(0.1)
+    def wait_for(self, dkrcmd, what, fail_msg, negative=False):
+        result = utils.wait_for(lambda: dkrcmd.stdout.find(what) > -1,
+                                5, step=0.1)
+        if negative is False:  # for clarity
+            self.failif(result is None, fail_msg)
         else:
-            self.failif(True, "Container '%s' didn't finish in 10s" % client)
+            self.failif(result > -1, fail_msg)  # negative test!
 
+    def run_once(self):
+        super(port, self).run_once()
+        params = self.sub_stuff['params']
+        # Server needs to be listening before client tries to connect
+        servercmd = self.start_server()
+        # Container running properly when python prompt appears
+        self.wait_for(servercmd, '>>> ', "No python prompt from server")
+        servercmd.stdin = str(self.python_server % params)  # str() for clarity
+        # Executed python prints this on stdout
+        self.wait_for(servercmd, 'Server Listening', "Server not listening")
+        clientcmd = self.start_client()
+        self.wait_for(clientcmd, '>>> ', "No python prompt from client")
+        clientcmd.stdin = str(self.python_client % params)
+        # Executed python includes printing this on stdout
+        self.wait_for(clientcmd, "Client Connecting", "No client connect")
+        # Client will probably exit first
+        self.sub_stuff['client_result'] = clientcmd.wait(5)
+        self.sub_stuff['server_result'] = servercmd.wait(5)
+        # Order shouldn't matter here
         clientcmd.close()
         servercmd.close()
 
-        self.sub_stuff['client_result'] = clientcmd.wait(1)
-        self.sub_stuff['server_result'] = servercmd.wait(1)
-
-    def check_env(self, exp, variable, env, container, appendix):
-        """
-        Check that variable's value matches the exp regexp. Fails the test
-        when it's not.
-        :param exp: Expected value (string)
-        :param variable: Name of the env variable (string)
-        :param env: Environment (dictionary)
-        :param container: Name of the container (string)
-        :param appendix: Rest of the error string
-        """
-        self.failif(not re.match(exp, env.get(variable)), "%s=%s not found in"
-                    " %s env:\n%s%s" % (variable, exp, container, env,
-                                        appendix))
-
     def postprocess(self):
-        def get_env(output):
-            for line in output.splitlines():
-                if line.startswith('ENVIRON = {'):
-                    return ast.literal_eval(line[10:])
-            self.failif(True, "Fail to get env from output\n%s" % output)
-
         super(port, self).postprocess()
         client_res = self.sub_stuff['client_result']
         server_res = self.sub_stuff['server_result']
         err_str = "\n\nServer\n%s\n\nClient\n%s" % (server_res, client_res)
         # server env
-        server_env = get_env(server_res.stdout)
+        server_env = self.get_env(server_res.stdout)
         self.check_env(self.sub_stuff['server_env'], 'ENV_VARIABLE',
                        server_env, 'server', err_str)
 
         # client env
-        client_env = get_env(client_res.stdout)
+        client_env = self.get_env(client_res.stdout)
         self.check_env(self.sub_stuff['client_env'], 'ENV_VARIABLE',
                        client_env, 'client', err_str)
         # linked client env
@@ -256,7 +286,7 @@ class port(run_env_base):
         self.check_env(self.sub_stuff['server_env'], 'SERVER_ENV_ENV_VARIABLE',
                        client_env, 'client', err_str)
         addr = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-        serv_port = str(self.sub_stuff['client_params']['port'])
+        serv_port = str(self.sub_stuff['params']['port'])
         self.check_env("tcp://%s:%s" % (addr, serv_port), 'SERVER_PORT',
                        client_env, 'client', err_str)
         self.check_env("tcp://%s:%s" % (addr, serv_port), 'SERVER_PORT_%s_TCP'
@@ -268,7 +298,7 @@ class port(run_env_base):
         self.check_env('tcp', 'SERVER_PORT_%s_TCP_PROTO' % serv_port,
                        client_env, 'client', err_str)
         # data on server
-        data = self.sub_stuff['client_params']['data']
+        data = self.sub_stuff['params']['data']
         exp = "RECEIVED: %s" % data
         self.failif(exp not in server_res.stdout, "Data '%s' sent from client "
                     "were not received on server:\n%s" % (exp, err_str))
