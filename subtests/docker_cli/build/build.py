f6d66ffc50335c9b0fb4fc5ce9c5f12da6e46995
Chris Evich <cevich@redhat.com>
2015-01-12 15:06:28 -0500

    subtests/build: Fix failing / error build tests
    *  Fix config. option subsubtests = ... rm_false_nocache -> cache
    *  Fix up whitespace/tab problems in code
    *  Fixed cleanup(), added cleanup() to subtest also (just in case)
    *  Fixed Dockerfile path problem
    *  Fixed regex search -> finditer typo
    *  Fixed dict copy typo
    *  Fixed many bugs in postprocess stages
    *  Fixed bug w/ truncating Dockerfile after replacing FROM
    *  Fixed bug w/ not restoring source content after modification
    *  Fixed module-level constants which only apply inside a class
    *  Fixed bundled Dockerfiles and content to work w/o busybox
    *  Fixed cleanup() not removing empty-name layers
    *  Fixed setup() not clearing srcdir before copying
    *  Fixed postprocess regex's not using re.MULTILINE
    *  Fixed bad_quiet postprocessing not checking stderr
    *  Added "pass" info log messages for each postprocess stage
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/build/build.py
+++ subtests/docker_cli/build/build.py
@@ -16,8 +16,6 @@ Operational Summary
 Prerequisites
 ----------------
 
-*  Tarballs bundled with the subtest
-*  Statically linked 'busybox' executable available on path or over HTTP
 *  Behavior in Dockerfiles match expected behavior of sub-subtest & config.
 """
 
@@ -25,7 +23,6 @@ import os.path
 import re
 import shutil
 from collections import namedtuple
-from urllib2 import urlopen
 from dockertest import subtest
 from dockertest.containers import DockerContainers
 from dockertest.dockercmd import DockerCmd
@@ -36,103 +33,102 @@ from dockertest.images import DockerImages
 from dockertest.output import OutputGood
 from dockertest.config import get_as_list
 
-# Generate named tuple class to hold fixed build parameters
-BuildDef = namedtuple('BuildDef',
-                      ['image_name',               # Name of image to build
-                       'dockercmd',                # Execution state
-                       'intermediate_containers',  # True/False/LAST
-                       'additional_containers',    # No. present after build
-                       'use_config_repo',          # True/False
-                       'dockerfile_dir_path',      # full path / url / git repo
-                       'base_repo_fqin',           # Substitute for FROM
-                       'positive_build_regex',     # Regex positive test
-                       'negative_build_regex'])    # Regex negative test
-
-RE_IMAGES = re.compile(r'\s*-+>\s*(\w{64}|\w{12})')
-RE_CONTAINERS = re.compile(r's*-+>\s*Running in\s*(\w{64}|\w{12})')
-
 
 class build(subtest.SubSubtestCaller):
 
+    def initialize(self):
+        super(build, self).initialize()
+        self.stuff['dc'] = dcont = DockerContainers(self)
+        self.stuff['existing_containers'] = dcont.list_container_ids()
+        self.stuff['di'] = dimg = DockerImages(self)
+        self.stuff['existing_images'] = dimg.list_imgs()
+
     def setup(self):
         super(build, self).setup()
-        # Must exist w/in directory holding Dockerfile
-        # Use local if possible
-        if os.path.exists('/usr/sbin/busybox'):
-            shutil.copy('/usr/sbin/busybox',
-                        os.path.join(self.srcdir, 'busybox'))
-        else:
-            urlstr = self.config['busybox_url']
-            urlstr = urlstr.strip()
-            self.logdebug("Downloading busybox from %s", urlstr)
-            resp = urlopen(urlstr, timeout=30)
-            data = resp.read()
-            busybox = os.open(os.path.join(self.srcdir, 'busybox'),
-                              os.O_WRONLY | os.O_CREAT, 0755)
-            os.write(busybox, data)
-            os.close(busybox)
-
         source_dirs = self.config['source_dirs']
         for dir_path in get_as_list(source_dirs):
             # bindir is location of this module
             src = os.path.join(self.bindir, dir_path)
             # srcdir is recreated if doesn't exist or if test version changes
             dst = os.path.join(self.srcdir, dir_path)
+            src = os.path.abspath(src)
+            dst = os.path.abspath(dst)
+            assert len(dst) > 5   # some minor protection
+            assert len(src) > 5
+            shutil.rmtree(dst, ignore_errors=True)
             shutil.copytree(src, dst)
-            # copy busybox
-            shutil.copy(os.path.join(self.srcdir, 'busybox'),
-                        os.path.join(dst, 'busybox'))
+
+    def cleanup(self):
+        super(build, self).cleanup()
+        # Auto-converts "yes/no" to a boolean
+        if self.config['remove_after_test']:
+            # Remove all previously non-existing containers
+            for cid in self.stuff['dc'].list_container_ids():
+                if cid in self.stuff['existing_containers']:
+                    continue    # don't remove previously existing ones
+                dcmd = DockerCmd(self, 'rm', ['--force', '--volumes', cid],
+                                 verbose=False)
+                dcmd.execute()
+            dimg = self.stuff['di']
+            # Remove all previously non-existing images
+            for img in dimg.list_imgs():
+                if img in self.stuff['existing_images']:
+                    continue
+                if img.full_name is None or img.full_name is '':
+                    thing = img.long_id
+                else:
+                    thing = img.full_name
+                dcmd = DockerCmd(self, 'rmi', ['--force', thing],
+                                 verbose=False)
+                dcmd.execute()
 
 
 class build_base(subtest.SubSubtest):
+
+    FROM_REGEX = re.compile(r'^FROM\s+.*', re.IGNORECASE | re.MULTILINE)
+    RE_IMAGES = re.compile(r'\s*-+>\s*(\w{64}|\w{12})', re.MULTILINE)
+    RE_CONTAINERS = re.compile(r's*-+>\s*Running in\s*(\w{64}|\w{12})',
+                               re.MULTILINE)
+    # Generate named tuple class to hold fixed build parameters
+    BuildDef = namedtuple('BuildDef',
+                          ['image_name',               # Name of image to build
+                           'dockercmd',                # Execution state
+                           'intermediate_containers',  # True/False/LAST
+                           'additional_containers',    # # present after build
+                           'use_config_repo',          # True/False
+                           'dockerfile_dir_path',      # full path, url, git
+                           'base_repo_fqin',           # Substitute for FROM
+                           'positive_build_regex',     # Regex positive test
+                           'negative_build_regex'])    # Regex negative test
 
     def dockerfile_dir_path(self, dir_path):
         if not isinstance(dir_path, basestring) or dir_path is None:
             return dir_path
         if dir_path[0] == '/':
             srcdir = self.parent_subtest.srcdir
-            abs_dir_path = os.path.join(srcdir, dir_path)
+            # Skip leading '/', append to source dir from setup()
+            abs_dir_path = os.path.join(srcdir, dir_path[1:])
             if os.path.isdir(abs_dir_path):
                 return abs_dir_path
             # else, fall through, return un-modified dir_path
         return dir_path
 
-    @classmethod
-    def dockerfile_token_value_finder(cls, dockerfile, token):
-        re_token = re.compile(r'%s\s+.*' % token)
-        re_token_value = re.compile(r'\s+.*')
-        re_token_comment = re.compile(r'^\#')
-        if re_token_comment.findall(dockerfile):
-            return ''
-        else:
-            token_with_value = re_token.findall(dockerfile)
-            token_value = re_token_value.findall(''.join(token_with_value))
-            return ''.join(token_value).strip(' ')
-
-    def dockerfile_repo_replace(self, path, repo):
-        dockerfile_new_data = ''
-        dockerfile_old = open(path, 'r')
-        try:
-            dockerfile_line = dockerfile_old.readlines()
-        finally:
-            dockerfile_old.close()
-        for token_line in dockerfile_line:
-            repo_old = self.dockerfile_token_value_finder(token_line, "FROM")
-            if repo_old:
-                token_line = token_line.replace(repo_old, repo)
-            dockerfile_new_data += token_line
-        dockerfile_new = open(path, 'w')
-        try:
-            dockerfile_new.write(dockerfile_new_data)
-        finally:
-            dockerfile_new.close()
+    def dockerfile_repo_replace(self, dockerfile_path, with_repo):
+        dockerfile = ''
+        with open(dockerfile_path, 'r') as dockerfile_old:
+            dockerfile = dockerfile_old.read()
+        dockerfile = self.FROM_REGEX.sub('FROM %s' % with_repo, dockerfile)
+        with open(dockerfile_path, 'w+b') as dockerfile_new:
+            dockerfile_new.write(dockerfile)
+        self.logdebug("Updated Dockerfile:")
+        self.logdebug(dockerfile)
 
     def initialize_utils(self):
         # Get the latest container (remove all newly created in cleanup
         self.sub_stuff['dc'] = dcont = DockerContainers(self)
         self.sub_stuff['existing_containers'] = dcont.list_container_ids()
         self.sub_stuff['di'] = dimg = DockerImages(self)
-        self.sub_stuff['existing_images'] = dimg.list_imgs_ids()
+        self.sub_stuff['existing_images'] = dimg.list_imgs()
 
     def make_builds(self, source):
         dimg = self.sub_stuff['di']
@@ -146,29 +142,28 @@ class build_base(subtest.SubSubtest):
         dockerfile_dir_path = source['dockerfile_dir_path'].strip()
         # Only replace w/ abs-path if dockerfile_dir_path starts with '/'
         # and if the abs-path points to an existing directory
-        dockerfile_dir_path = dockerfile_dir_path(dockerfile_dir_path)
+        dockerfile_dir_path = self.dockerfile_dir_path(dockerfile_dir_path)
         if use_config_repo:  # Indicates NOT a url / git repo
             # Form full path including "Dockerfile"
             full_dockerfile_path = os.path.join(dockerfile_dir_path,
                                                 'Dockerfile')
             self.dockerfile_repo_replace(full_dockerfile_path,
                                          base_repo_fqin)
-            del full_dockerfile_path  # no longer needed
         # else:  dockerfile_dir_path is a url or git repo, read-only
         docker_build_options = source['docker_build_options'].strip()
         subargs = get_as_list(docker_build_options) + ["-t", image_name,
                                                        dockerfile_dir_path]
         dockercmd = DockerCmd(self, 'build', subargs, verbose=True)
         # Pass as keywords allows ignoring parameter order
-        return [BuildDef(image_name=image_name,
-                         dockercmd=dockercmd,
-                         intermediate_containers=intermediate_containers,
-                         additional_containers=additional_containers,
-                         use_config_repo=use_config_repo,
-                         dockerfile_dir_path=dockerfile_dir_path,
-                         base_repo_fqin=base_repo_fqin,
-                         positive_build_regex=positive_build_regex,
-                         negative_build_regex=negative_build_regex)]
+        return [self.BuildDef(image_name=image_name,
+                              dockercmd=dockercmd,
+                              intermediate_containers=intermediate_containers,
+                              additional_containers=additional_containers,
+                              use_config_repo=use_config_repo,
+                              dockerfile_dir_path=dockerfile_dir_path,
+                              base_repo_fqin=base_repo_fqin,
+                              positive_build_regex=positive_build_regex,
+                              negative_build_regex=negative_build_regex)]
 
     def initialize(self):
         super(build_base, self).initialize()
@@ -191,21 +186,23 @@ class build_base(subtest.SubSubtest):
             output = build_def.dockercmd.stdout
         # Fail if does not match positive regex
         if build_def.positive_build_regex != '':
-            positive_regex = re.compile(build_def.positive_build_regex)
+            positive_regex = re.compile(build_def.positive_build_regex,
+                                        re.MULTILINE)
             mobj = positive_regex.search(output)
             self.failif(mobj is None,
-                        "Positive message not found in output")
+                        "Positive regex not found in output")
         # Fail if match negative regex
         if build_def.negative_build_regex != '':
-            negative_regex = re.compile(build_def.negative_build_regex)
+            negative_regex = re.compile(build_def.negative_build_regex,
+                                        re.MULTILINE)
             mobj = negative_regex.search(output)
             self.failif(mobj is not None,
-                        "Negative message found in output")
+                        "Negative regex found in output")
         self.loginfo("Positive test checks pass")
 
     def postprocess_negative(self, build_def, check_stderr=False):
         # Verify exit status
-        mustfail(build_def.dockercmd.cmdrsult)
+        mustfail(build_def.dockercmd.cmdresult)
         # Only check for panic or usage message
         OutputGood(build_def.dockercmd.cmdresult, skip='error_check')
         if check_stderr:
@@ -214,40 +211,55 @@ class build_base(subtest.SubSubtest):
             output = build_def.dockercmd.stdout
         # Fail if match positive regex
         if build_def.positive_build_regex != '':
-            positive_regex = re.compile(build_def.positive_build_regex)
+            positive_regex = re.compile(build_def.positive_build_regex,
+                                        re.MULTILINE)
             mobj = positive_regex.search(output)
             # reverse of postprocess_positive()
             self.failif(mobj is not None,
-                        "Positive message found in output")
+                        "Positive regex found in output")
         # Fail if does not match negative regex
         if build_def.negative_build_regex != '':
-            negative_regex = re.compile(build_def.negative_build_regex)
+            negative_regex = re.compile(build_def.negative_build_regex,
+                                        re.MULTILINE)
             mobj = negative_regex.search(output)
             # reverse of postprocess_positive()
             self.failif(mobj is None,
-                        "Negative message not found in output")
+                        "Negative regex not found in output")
         self.loginfo("Negative test checks pass")
 
-    def postprocess_created_images(self, build_def):
+    def postprocess_created_images(self, build_def, negative=False):
         dkrimgs = self.sub_stuff['di']
         dkrimgs.images_args += " --all"    # list all
         images = dkrimgs.list_imgs()
         dkrimgs.images_args = DockerImages.images_args
 
-        img_names = [di.full_name for di in images]
-        imf_ids = [di.short_id for di in images]
-        self.failif(build_def.image_name not in img_names,
-                    "Test image '%s' not found after build"
-                    % build_def.image_name)
+        # Search all images, ignore unknown (None) name components
+        matches = [img
+                   for img in images
+                   if img.cmp_greedy_full_name(build_def.image_name)]
+        self.logdebug("Current images:\n%s",
+                      '\n'.join([img.full_name
+                                 for img in images
+                                 if img.full_name is not '']))
+        if negative:
+            self.failif(len(matches) != 0,
+                        "Unexpected test image '%s' found"
+                        % (build_def.image_name))
+        else:  # positive
+            self.failif(len(matches) != 1,
+                        "Test image '%s' not found"
+                        % (build_def.image_name))
 
         stdout = build_def.dockercmd.stdout
         created_ids = [mobj.group(1)
-                       for mobj in RE_IMAGES.finditer(stdout)
+                       for mobj in self.RE_IMAGES.finditer(stdout)
                        if mobj is not None]
+        img_ids = [img.short_id for img in images]
         for created_id in created_ids:
-            self.failif(created_id not in imf_ids,
+            self.failif(created_id not in img_ids,
                         "Intermediate image %s not found after build"
                         % created_id)
+        self.loginfo("Images checks pass")
 
     def postprocess_intermediate_last(self, containers, created_containers):
         # Only last one should be present
@@ -283,8 +295,8 @@ class build_base(subtest.SubSubtest):
 
         stdout = build_def.dockercmd.stdout
         created_containers = [mobj.group(1)
-                              for mobj in RE_CONTAINERS.search(stdout)
-                              if mobj is not None]
+                              for mobj in self.RE_CONTAINERS.finditer(stdout)
+                              if mobj]
 
         if build_def.intermediate_containers == 'LAST':
             self.postprocess_intermediate_last(containers, created_containers)
@@ -294,6 +306,7 @@ class build_base(subtest.SubSubtest):
             self.postprocess_intermediate_false(containers, created_containers)
         else:
             self.logdebug("Not checking intermediate containers")
+        self.loginfo("Containers checks pass")
 
     def postprocess_container_count(self, build_def):
         expected = int(build_def.additional_containers)
@@ -303,6 +316,7 @@ class build_base(subtest.SubSubtest):
         self.failif(diff != expected,
                     "Expected %d containers, found %d"
                     % (expected, diff))
+        self.loginfo("Containers count pass")
 
     def postprocess_result(self, build_def):
         self.postprocess_positive(build_def)
@@ -313,11 +327,14 @@ class build_base(subtest.SubSubtest):
     def postprocess(self):
         super(build_base, self).postprocess()
         for build_def in self.sub_stuff['builds']:
+            self.loginfo("Checking build from %s"
+                         % build_def.dockerfile_dir_path)
             self.postprocess_result(build_def)
-        self.postprocess_no_containers()
 
     def cleanup(self):
         super(build_base, self).cleanup()
+        # Some of this could have been modified, recover from source
+        self.parent_subtest.setup()
         # Auto-converts "yes/no" to a boolean
         if self.config['remove_after_test']:
             # Remove all previously non-existing containers
@@ -329,10 +346,16 @@ class build_base(subtest.SubSubtest):
                 dcmd.execute()
             dimg = self.sub_stuff['di']
             # Remove all previously non-existing images
-            for img in dimg.list_imgs_ids():
+            for img in dimg.list_imgs():
                 if img in self.sub_stuff['existing_images']:
                     continue
-                dimg.remove_image_by_id(img)
+                if img.full_name is None or img.full_name is '':
+                    thing = img.long_id
+                else:
+                    thing = img.full_name
+                dcmd = DockerCmd(self, 'rmi', ['--force', thing],
+                                 verbose=False)
+                dcmd.execute()
 
 
 class local_path(build_base):
@@ -351,13 +374,18 @@ class bad(build_base):  # negative test
 
     def postprocess_result(self, build_def):
         self.postprocess_negative(build_def)
-        self.postprocess_created_images(build_def)
+        self.postprocess_created_images(build_def, negative=True)
         self.postprocess_created_containers(build_def)
         self.postprocess_container_count(build_def)
 
 
 class bad_quiet(bad):  # negative test
-    pass
+
+    def postprocess_result(self, build_def):
+        self.postprocess_negative(build_def, check_stderr=True)
+        self.postprocess_created_images(build_def, negative=True)
+        self.postprocess_created_containers(build_def)
+        self.postprocess_container_count(build_def)
 
 
 class bad_force_rm(bad):  # negative test
@@ -371,8 +399,8 @@ class rm_false(build_base):
 class cache(build_base):
 
     def make_builds(self, source):
-        first = source.deepcopy()
-        second = source.deepcopy()
+        first = source.copy()
+        second = source.copy()
         # Just remote all key2's to keep tidy
         for key in first.keys():  # dict is being modified
             if key.endswith('2'):
