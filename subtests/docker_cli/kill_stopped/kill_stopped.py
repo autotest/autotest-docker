0d8b3ce3af92579c0e2d831151ea460bda17476a
Chris Evich <cevich@redhat.com>
2015-02-09 13:38:43 -0500

    kill_stress: Fix improper tests
    Removed all tty enabled sub-subtests as they are now invalid.  Also
    added signal 18/19/20 to the skip list since it cannot be reliably tested.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/kill_stopped/kill_stopped.py
+++ subtests/docker_cli/kill_stopped/kill_stopped.py
@@ -13,9 +13,8 @@ Operational Summary
 4. send SIGCONT
 5. check received signals
 """
-import random
 
-from kill_utils import kill_check_base, DockerCmd
+from kill_utils import kill_check_base
 from dockertest import subtest
 
 
@@ -25,46 +24,7 @@ class kill_stopped(subtest.SubSubtestCaller):
 
 
 class sigstop(kill_check_base):
-
-    """
-    Test usage of docker 'kill' command (stopped container)
-
-    initialize:
-    1) start VM with test command
-    2) create sequence of signals in this manner (uses numeric values):
-       [SIGSTOP, 1, 2, 3, .., 8, 10, .., 16, SIGSTOP, 20, .., 31, SIGCONT, 9]
-    run_once:
-    3) execute series of kill signals followed with the output check
-    3b) in case of SIGSTOP it stores following signals until SIGCONT and
-        verifies they were all handled properly
-    4) sends docker kill -9 and verifies docker was killed
-    postprocess:
-    5) analyze results
-    """
-
-    def _populate_kill_cmds(self, extra_subargs):
-        signals_sequence = range(1, 32)
-        signals_sequence.remove(9)
-        signals_sequence.remove(17)
-        signals_sequence.remove(18)
-
-        # TODO: Should these be ignored? They can be caught when not STOPPED
-        # signals_sequence.remove(20)
-        # signals_sequence.remove(21)
-        # signals_sequence.remove(22)
-
-        signals_sequence = [19] + signals_sequence + [18, 9]
-        kill_cmds = []
-        for signal in signals_sequence:
-            subargs = (["%s%s" % (random.choice(('-s ', '--signal=')), signal)]
-                       + extra_subargs)
-            cmd = DockerCmd(self, 'kill', subargs, verbose=False)
-            kill_cmds.append(cmd)
-
-        self.logdebug("kill_command_example: %s", kill_cmds[0])
-        self.logdebug("signals_sequence: %s", signals_sequence)
-        self.sub_stuff['signals_sequence'] = signals_sequence
-        self.sub_stuff['kill_cmds'] = kill_cmds
+    pass
 
 
 class sigstop_ttyoff(sigstop):
@@ -73,12 +33,7 @@ class sigstop_ttyoff(sigstop):
     tty = False
 
 
-class sigstop_sigproxy(sigstop):
-
-    """ Direct kill variant of the sigstop test """
-
-
-class sigstop_sigproxy_ttyoff(sigstop_sigproxy):
+class sigstop_sigproxy_ttyoff(sigstop):
 
     """ Non-tty variant of the sigstop_sigproxy test """
     tty = False
