51d31cc996989cb804ad4e3a915e1c212f5a76f0
Chris Evich <cevich@redhat.com>
2015-02-26 12:59:10 -0500

    build: Move cache test to separate module
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/build/build.py
+++ subtests/docker_cli/build/build.py
@@ -528,25 +528,9 @@ class bad_force_rm(build_base):  # negative test
 class rm_false(build_base):
     pass
 
+
 class jsonvol(build_base):
     pass
-
-class cache(build_base):
-
-    def make_builds(self, source):
-        first = source.copy()
-        second = source.copy()
-        # Just remote all key2's to keep tidy
-        for key in first.keys():  # dict is being modified
-            if key.endswith('2'):
-                del first[key]
-        # Move key2 values, remove key2 to keep tidy
-        for key in second.keys():  # move '2' key valus
-            if key.endswith('2'):
-                second[key[:-1]] = second[key]  # copy value
-                del second[key]  # remove 2 key
-        super_make_builds = super(cache, self).make_builds
-        return super_make_builds(first) + super_make_builds(second)
 
 
 class dockerignore(build_base):
