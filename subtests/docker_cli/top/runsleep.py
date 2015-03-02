a518dc23c1963e5999b22fd66efd70ad9a93f964
Chris Evich <cevich@redhat.com>
2015-03-02 16:37:34 -0500

    top: Re-write top test
    Original version was not working and unmaintainable.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ subtests/docker_cli/top/runsleep.py
@@ -0,0 +1,40 @@
+from top import base
+from dockertest.dockercmd import DockerCmd
+from dockertest.output import OutputGood
+from dockertest.output import TextTable
+
+
+class runsleep(base):
+
+    COMMAND = 'sleep 5m'
+
+    def init_run_dkrcmd(self):
+        subargs = self.sub_stuff['run_options']
+        subargs += [self.sub_stuff['fqin'], self.COMMAND]
+        return DockerCmd(self, 'run', subargs)
+
+    def init_top_dkrcmd(self):
+        subargs = [self.get_run_name()]
+        subargs += self.sub_stuff['top_options']
+        return DockerCmd(self, 'top', subargs)
+
+    def get_run_name(self):
+        if self.sub_stuff.get('_name') is None:
+            dc = self.sub_stuff['dc']
+            name = dc.get_unique_name()
+            self.sub_stuff['_name'] = name
+        return self.sub_stuff.get('_name')
+
+    def run_once(self):
+        super(runsleep, self).run_once()
+        self.sub_stuff['top_dkrcmd'].execute()  # blocking
+
+    def postprocess(self):
+        OutputGood(self.sub_stuff['run_dkrcmd'].cmdresult)
+        OutputGood(self.sub_stuff['top_dkrcmd'].cmdresult)
+        pstable = TextTable(self.sub_stuff['top_dkrcmd'].stdout)
+        self.failif(len(pstable) != 1)
+        psrow = pstable[0]
+        self.failif(psrow['USER'] != 'root')
+        self.failif(int(psrow['PID']) == 1)
+        self.failif(psrow['COMMAND'] != self.COMMAND)
