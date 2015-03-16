167c8291207d25e01151c866c74b08c55b82a00e
Chris Evich <cevich@redhat.com>
2015-03-16 19:11:21 +0000

    Atomic: Condition Setup
    Don't setup a RUN label if image already hase one.
    
    Produce a basic custom config, if one doesn't exist.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- atomic/atomic_setup.sh
+++ atomic/atomic_setup.sh
@@ -35,16 +35,16 @@ cd "${TMPDIR}"
 if [ "$?" -ne 0 ]; then exit 1; fi
 
 RUNSCRIPT="${HOST_ROOT}/${NAME}${DOCKER_AUTOTEST_PATH}/atomic/atomic_run.sh"
-if [ -e "$HOST$RUNSCRIPT" ]
+if [ -f "$HOST$RUNSCRIPT" ]
 then
-    echo -e "\n Existing host $HOST$RUNSCRIPT found, not overwriting..."
+    echo -e "\nExisting installation found, not overwriting..."
 else
     # Docker autotest exercises docker, so cannot be run under docker.
-    echo -e "\nTransfering image contents to host's ${HOST_ROOT}/${NAME}..."
-
-    mkdir -vp ${HOST}${HOST_ROOT}/${NAME} &> ${OUTPUT}
+    echo -e "\nCreating ${HOST_ROOT}/${NAME}" &> ${OUTPUT}
+    mkdir -p ${HOST}${HOST_ROOT}/${NAME}
     if [ "$?" -ne 0 ]; then exit 1; fi
 
+    echo -e "\nTransfering image contents to host's ${HOST_ROOT}/${NAME}..."
     # Need container to extract filesystem
     ${HOST}/usr/bin/docker run --name ${TMPNAME} ${IMAGE} true &> ${OUTPUT}
     if [ "$?" -ne 0 ]; then exit 1; fi
@@ -55,18 +55,40 @@ else
     if [ "$?" -ne 0 ]; then exit 1; fi
 fi
 
-# Add a layer ontop of original build image for starting docker autotest
-OLDIID=$(${HOST}/usr/bin/docker inspect --format "{{.Id}}" ${IMAGE} | cut -c 1-12)
-if [ "$?" -ne 0 ]; then exit 1; fi
-${HOST}/usr/bin/docker build ${QUIET} -t ${IMAGE} - <<EOF
+# Check if install/setup was already run
+if ! ${HOST}/usr/bin/docker inspect --format '{{.Config.Labels.RUN}}' ${IMAGE} | \
+     grep -q '/usr/bin/docker'
+then
+    echo -e "\nBuilding runtime onto base image..." &> ${OUTPUT}
+    # Add a layer ontop of original build image for starting docker autotest
+    OLDIID=$(${HOST}/usr/bin/docker inspect --format "{{.Id}}" ${IMAGE} | cut -c 1-12)
+    if [ "$?" -ne 0 ]; then exit 1; fi
+    ${HOST}/usr/bin/docker build ${QUIET} -t ${IMAGE} - <<EOF
 FROM ${OLDIID}
-LABEL INSTALL=""
 LABEL RUN="${RUNSCRIPT} ${NAME} ${AUTOTEST_PATH} ${DOCKER_BIN_PATH}"
 LABEL UNINSTALL="rm -rf ${HOST_ROOT}/${NAME}"
 EOF
-if [ "$?" -ne 0 ]; then exit 1; fi
+    if [ "$?" -ne 0 ]; then exit 1; fi
+else
+    echo -e "\nExisting runtime image detected, not rebuilding."
+fi
 
-# Tag former first stage w/ name pointing to second-stage ID
-#NEWIID=$(${HOST}/usr/bin/docker inspect --format "{{.Id}}" ${IMAGE} | cut -c 1-12)
-#${HOST}/usr/bin/docker tag ${OLDIID} ${NAME}:${NEWIID}
-#if [ "$?" -ne 0 ]; then exit 1; fi
+# Setup basic configuration if doesn't exist
+CONFIGCUSTOM="${HOST}${HOST_ROOT}/${NAME}${DOCKER_AUTOTEST_PATH}/config_custom"
+CONFIGDEFAULTS="${HOST}${HOST_ROOT}/${NAME}${DOCKER_AUTOTEST_PATH}/config_defaults"
+# Build basic configuration if doesn't exist
+if [ -f "${CONFIGCUSTOM}/defaults.ini" ] || [ -f "${CONFIGCUSTOM}/control.ini" ]
+then
+    echo -e "\nNot overwriting existing configuration."
+else
+    echo -e "\nCreating configuration in ${HOST_ROOT}/${NAME}${DOCKER_AUTOTEST_PATH}/config_custom"
+    cp ${CONFIGDEFAULTS}/control.ini ${CONFIGCUSTOM}
+    cp ${CONFIGDEFAULTS}/defaults.ini ${CONFIGCUSTOM}
+    for ini in $(grep --files-with-matches -r __example__ \
+                 --exclude defaults.ini \
+                 --exclude subtests/example.ini ${CONFIGDEFAULTS})
+    do
+        cat $ini
+        echo
+    done >> ${CONFIGCUSTOM}/test_config.ini
+fi
