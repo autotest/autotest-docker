b21146218814cd6a613674f220d09041f5252898
Christopher Evich <cevich@redhat.com>
2015-10-28 12:14:28 -0400

    version: Fix parsing > 1.7.1 version table
    Preserves old behavior and supports new table format
    
    Signed-off-by: Christopher Evich <cevich@redhat.com>

--- dockertest/output.py
+++ dockertest/output.py
@@ -25,21 +25,24 @@ class DockerVersion(object):
     #: Raw, possibly empty or multi-line output from docker version command.
     #: Read-only, set in __init__
     version_string = None
-    #: Line-split version of version_string, not stripped, read-only, set in
-    #: __init__
-    version_lines = None
+    #: Various cache's for properties (do not use)
     _client = None
     _server = None
+    _client_lines = None
+    _server_lines = None
+    _client_info = None
+    _server_info = None
 
     def __init__(self, version_string):
         self.version_string = version_string
-        self.version_lines = self.version_string.splitlines()
 
-    @property
-    def client(self):
-        """
-        Read-only property representing version-number string of docker client
-        """
+    def _oops(self, what):
+        raise xceptions.DockerOutputError("Couldn't parse %s "
+                                          "from %s" % (what,
+                                                       self.version_string))
+
+    # This is old code, not updating to preserve old behavior
+    def _old_client(self):
         if self._client is None:
             regex = re.compile(r'Client\s+version:\s+(\d+\.\d+\.\d+\S*)',
                                re.IGNORECASE)
@@ -49,15 +52,11 @@ class DockerVersion(object):
                 if bool(mobj):
                     self._client = mobj.group(1)
         if self._client is None:
-            raise xceptions.DockerOutputError("Couldn't parse client version "
-                                              "from %s" % self.version_string)
+            self._oops('client version')
         return self._client
 
-    @property
-    def server(self):
-        """
-        Read-only property representing version-number string of docker server
-        """
+    # This is old code, not updating to preserve old behavior
+    def _old_server(self):
         if self._server is None:
             regex = re.compile(r'Server\s*version:\s*(\d+\.\d+\.\d+\S*)',
                                re.IGNORECASE)
@@ -67,8 +66,130 @@ class DockerVersion(object):
                 if bool(mobj):
                     self._server = mobj.group(1)
         if self._server is None:
-            raise xceptions.DockerOutputError("Couldn't parse server version "
-                                              "from %s" % self.version_string)
+            self._oops('server version')
+        return self._server
+
+    def _split_client_server(self):
+        # Split the raw string into client & server sections
+        client_lines = []
+        server_lines = []
+        version_lines = list(self.version_lines)  # work on a copy
+        version_lines.reverse()  # start at beginning
+        while version_lines:
+            version_line = version_lines.pop().strip()
+            if version_line == '':
+                continue
+            elif version_line.find('Client:') > -1:
+                # Don't assume which section came first
+                while version_lines and version_lines[-1].find('Server:') < 0:
+                    version_line = version_lines.pop().strip()
+                    if version_line == '':
+                        continue
+                    client_lines.append(version_line)
+                continue
+            elif version_line.find('Server:') > -1:
+                # Don't assume which section came first
+                while version_lines and version_lines[-1].find('Client:') < 0:
+                    version_line = version_lines.pop().strip()
+                    if version_line == '':
+                        continue
+                    server_lines.append(version_line)
+                continue
+            else:
+                msg = ("Unexpected line '%s' in version string: '%s'"
+                       % (version_line, self.version_string))
+                raise xceptions.DockerOutputError(msg)
+        return (client_lines, server_lines)
+
+    # This is to preserve API behavior for old tests
+    @property
+    def version_lines(self):
+        """Read-only property that returns all lines in version table"""
+        return self.version_string.splitlines()
+
+    @property
+    def client_lines(self):
+        """
+        Read-only property of split/stripped client section of version string
+        """
+        if not self._client_lines:
+            (self._client_lines,
+             self._server_lines) = self._split_client_server()
+        return self._client_lines
+
+    @property
+    def server_lines(self):
+        """
+        Read-only property of split/stripped server section of version string
+        """
+        if not self._server_lines:
+            (self._client_lines,
+             self._server_lines) = self._split_client_server()
+        return self._server_lines
+
+    def _info(self, is_client, key):
+        key = key.strip()
+        if is_client:
+            infodict = self._client_info
+            infolines = self.client_lines
+        else:
+            infodict = self._server_info
+            infolines = self.server_lines
+        try:
+            return infodict[key.strip().lower()]
+        except TypeError:  # infodict == None
+            if is_client:
+                self._client_info = {}
+            else:
+                self._server_info = {}
+            return self._info(is_client, key)
+        except KeyError:  # infodict == empty
+            if not infodict:
+                for line in infolines:
+                    try:
+                        _key, value = line.strip().lower().split(':', 1)
+                    except ValueError:
+                        raise ValueError("Error splitting info line '%s'"
+                                         % line)
+                    infodict[_key.strip()] = value.strip()
+                return self._info(is_client, key)
+            else:  # avoid infinite recursion
+                self._oops("key %s" % key)
+
+    def client_info(self, key):
+        """Return item named 'key' from client section of version info table"""
+        return self._info(True, key)
+
+    def server_info(self, key):
+        """Return item named 'key' from server section of version info table"""
+        return self._info(False, key)
+
+    @property
+    def client(self):
+        """
+        Read-only property representing version-number string of docker client
+        """
+        if self._client is None:
+            try:
+                return self._old_client()
+            except xceptions.DockerOutputError:
+                self._client = self.client_info('version')
+        if self._client is None:
+            self._oops('client version')
+        return self._client
+
+    @property
+    def server(self):
+        """
+        Read-only property representing version-number string of docker server
+        """
+        if self._server is None:
+            try:
+                return self._old_server()
+            except xceptions.DockerOutputError:
+                self._server = self.server_info('version')
+        if self._server is None:
+            self._oops('server version')
         return self._server
 
 
