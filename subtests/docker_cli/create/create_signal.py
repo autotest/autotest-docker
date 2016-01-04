46ca369e65c137cc7564ac73cb53b6e56348d7f0
Chris Evich <cevich@redhat.com>
2016-01-04 13:10:25 -0500

    Raise test NA if docker version is too old for test
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/create/create_signal.py
+++ subtests/docker_cli/create/create_signal.py
@@ -3,16 +3,38 @@ Test sending signal to created but not started container exits non-zero.
 """
 
 import signal
+# FIXME: distutils.version is incorrectly missing in Travis CI, disable warning
+from distutils.version import LooseVersion  # pylint: disable=E0611
 from dockertest.dockercmd import DockerCmd
 from dockertest.output import mustfail
+from dockertest.output import mustpass
 from dockertest.output import OutputNotBad
+from dockertest.output import DockerVersion
+from dockertest.xceptions import DockerTestNAError
 from create import create_base
 
 
 class create_signal(create_base):
 
+    #: The exit behavior changed in docker version and later
+    non_zero_exit_version = "1.8.2"
+
+    def is_newer_docker(self):
+        ver_stdout = mustpass(DockerCmd(self, "version").execute()).stdout
+        dv = DockerVersion(ver_stdout)
+        client_version = LooseVersion(dv.client)
+        dep_version = LooseVersion(self.non_zero_exit_version)
+        if client_version < dep_version:
+            return False
+        else:
+            return True
+
     def initialize(self):
         super(create_signal, self).initialize()
+        if not self.is_newer_docker():
+            raise DockerTestNAError("Test does not support "
+                                    "docker version < %s",
+                                    self.non_zero_exit_version)
         self.sub_stuff['sigdkrcmd'] = None
 
     def run_once(self):
