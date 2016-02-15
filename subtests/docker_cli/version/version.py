b8debe05c43d32dc1c95bf87599db02ab264284d
Chris Evich <cevich@redhat.com>
2016-02-15 19:51:35 +0000

    version test should write info into sysinfo dir
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- subtests/docker_cli/version/version.py
+++ subtests/docker_cli/version/version.py
@@ -17,6 +17,7 @@ Prerequisites
 None
 """
 
+import os.path
 from dockertest import subtest
 from dockertest.output import OutputGood
 from dockertest.output import DockerVersion
@@ -41,8 +42,12 @@ class version(subtest.Subtest):
         # Raise exception on Go Panic or usage help message
         outputgood = OutputGood(self.stuff['cmdresult'])
         docker_version = DockerVersion(outputgood.stdout_strip)
-        self.loginfo("Found docker versions client: %s server %s ",
-                     docker_version.client, docker_version.server)
+        info = ("docker version client: %s server %s"
+                % (docker_version.client, docker_version.server))
+        self.loginfo("Found %s", info)
+        with open(os.path.join(self.job.sysinfo.sysinfodir,
+                               'docker_version'), 'wb') as info_file:
+            info_file.write("%s\n" % info)
         self.verify_version(docker_version)
 
     def verify_version(self, docker_version):
