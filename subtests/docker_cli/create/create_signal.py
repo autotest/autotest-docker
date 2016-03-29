499b610ad427f2fcd5d6a507a9ccc763ab97363e
Ed Santiago <santiago@redhat.com>
2016-03-29 15:55:48 -0600

    Refactor: new subtest.require_docker_version()
    Raises DockerTestNAError if currently-installed docker
    version is less than a specified requirement.
    
    Also: removed obsolete version checks and code from rmi
    
    Signed-off-by: Ed Santiago <santiago@redhat.com>

--- subtests/docker_cli/create/create_signal.py
+++ subtests/docker_cli/create/create_signal.py
@@ -3,15 +3,9 @@ Test sending signal to created but not started container exits non-zero.
 """
 
 import signal
-# There is a bug in Pylint + virtualenv that makes this fail in Travis CI
-# https://github.com/PyCQA/pylint/issues/73
-from distutils.version import LooseVersion  # pylint: disable=E0611
 from dockertest.dockercmd import DockerCmd
 from dockertest.output import mustfail
-from dockertest.output import mustpass
 from dockertest.output import OutputNotBad
-from dockertest.output import DockerVersion
-from dockertest.xceptions import DockerTestNAError
 from create import create_base
 
 
@@ -20,22 +14,9 @@ class create_signal(create_base):
     #: The exit behavior changed in docker version and later
     non_zero_exit_version = "1.8.2"
 
-    def is_newer_docker(self):
-        ver_stdout = mustpass(DockerCmd(self, "version").execute()).stdout
-        dv = DockerVersion(ver_stdout)
-        client_version = LooseVersion(dv.client)
-        dep_version = LooseVersion(self.non_zero_exit_version)
-        if client_version < dep_version:
-            return False
-        else:
-            return True
-
     def initialize(self):
         super(create_signal, self).initialize()
-        if not self.is_newer_docker():
-            raise DockerTestNAError("Test does not support "
-                                    "docker version < %s",
-                                    self.non_zero_exit_version)
+        self.require_docker_version(self.non_zero_exit_version)
         self.sub_stuff['sigdkrcmd'] = None
 
     def run_once(self):
