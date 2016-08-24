a983e6e58b1e0571b01ede27db9e3742e308b766
Ed Santiago <santiago@redhat.com>
2016-08-24 09:36:23 -0600

    review feedback from cevich
     - remove unused subclass & method
     - (major): refactor & robustify, so we no longer depend on
       sleep & timing.
    
    Signed-off-by: Ed Santiago <santiago@redhat.com>

--- subtests/docker_cli/namespaces/namespaces.py
+++ subtests/docker_cli/namespaces/namespaces.py
@@ -11,8 +11,10 @@ Operational Summary
 #. See individual tests.
 """
 
+import os
 from autotest.client import utils
 from dockertest import subtest
+from dockertest.containers import DockerContainers
 from dockertest.dockercmd import AsyncDockerCmd
 from dockertest.images import DockerImage
 from dockertest.subtest import SubSubtest
@@ -22,11 +24,7 @@ class namespaces(subtest.SubSubtestCaller):
     pass
 
 
-class namespaces_base(SubSubtest):
-    pass
-
-
-class rmdir_mount(namespaces_base):
+class rmdir_mount(SubSubtest):
     """
     Motivation for this test was the August 2016 attempt to package
     docker-latest-1.12: the initial builds didn't work, docker daemon
@@ -39,23 +37,42 @@ class rmdir_mount(namespaces_base):
     without MountFlags=slave. See rhbz1347821 for further info.
     """
 
-    def initialize(self):
-        super(rmdir_mount, self).initialize()
-
     def run_once(self):
         """
-        Run a background container with --rm; while that's running,
-        unshare its mount namespace in a longer-running process.
-        When the container finishes, we expect the --rm to succeed.
+        Run a background container with --rm; while that's running, clone
+        another process sharing the same mount namespace. That process
+        will signal the container to finish, and when docker cleans it
+        up (--rm) we expect no errors.
         """
         super(rmdir_mount, self).run_once()
         fin = DockerImage.full_name_from_defaults(self.config)
-        subargs = ['--rm', fin, 'bash', '-c', '"echo READY; sleep 15"']
+        name = DockerContainers(self).get_unique_name()
+
+        # Create a state file; container will exit once this is deleted
+        waitfile = 'state_file_for_signaling_the_container_to_exit'
+        waitfile_local = os.path.join(self.tmpdir, waitfile)
+        open(waitfile_local, 'a').close()
+
+        # Basically: mount self.tmpdir as /tmp in container, constantly
+        # check the state file, exit when it's deleted, then (--rm) have
+        # docker clean it up.
+        subargs = ['--rm', '-v', '%s:/tmp:z' % self.tmpdir,
+                   '--name', name, fin,
+                   'bash', '-c',
+                   "'echo READY; while [ -e /tmp/%s ]; do sleep 0.1; done'"
+                   % waitfile]
         dkrcmd = AsyncDockerCmd(self, 'run', subargs)
         self.sub_stuff['dkrcmd'] = dkrcmd
         dkrcmd.execute()
-        dkrcmd.wait_for_ready(timeout=5)
-        self.sub_stuff['unshare'] = utils.run("unshare -m bash -c 'sleep 20'")
+        dkrcmd.wait_for_ready(name, timeout=5)
+
+        # Container is running. Now, in parallel: clone, delete state file,
+        # and wait for container to terminate.
+        in_n = ("rm -f %s;"
+                " while :;do"
+                "   docker inspect %s 2>/dev/null || exit 0;"
+                " done" % (waitfile_local, name))
+        self.sub_stuff['unshare'] = utils.run("unshare -m bash -c '%s'" % in_n)
 
     def postprocess(self):
         """
