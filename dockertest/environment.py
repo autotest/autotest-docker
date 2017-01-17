eae12d6f1a6dbbc69910ffd758bf298bb89af623
Chris Evich <cevich@redhat.com>
2017-01-17 10:31:43 -0500

    Resolve set_selinux_context() FIXME
    ``pwd`` is misleading, it should be ``path`` - which also matches
    ``get_selinux_context()``.
    
    Making ``pwd`` a keyword argument preserves compatibility with all
    prior callers that used it as a keyword argument.  Since it was
    the only positional argument, replacing it with a new first positional
    keyword ``path`` argument also helps preserve behavior with old callers.
    
    Having ``path`` default to ``None``, catches any new callers that use
    keyword arguments, but fail to pass in ``pwd``.  In all cases, if ``pwd``
    is used, issue a deprecation warning so callers don't use it
    accidentally in new code.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- dockertest/environment.py
+++ dockertest/environment.py
@@ -7,19 +7,32 @@ Low-level/standalone host-environment handling/checking utilities/classes/data
        in autotest!
 """
 
+import warnings
 import subprocess
 import selinux
 
 
-# FIXME: pwd is misleading, it should be 'path'
-def set_selinux_context(pwd, context=None, recursive=True):
+def set_selinux_context(path=None, context=None, recursive=True, pwd=None):
     """
     When selinux is enabled it sets the context by chcon -t ...
-    :param pwd: target directory
+    :param path: Relative/absolute path to file/directory to modify
     :param context: desired context (svirt_sandbox_file_t by default)
     :param recursive: set context recursively (-R)
+    :param pwd: target path (deprecated, was first argument name)
     :raise OSError: In case of failure
     """
+    # This is here to catch any callers using pwd as a keyword argument.
+    # Moving it to the end of the list is safe because there only ever
+    # was a single positional argument in this function (preserves API)
+    if pwd is not None:
+        warnings.warn("Use of the pwd argument is deprecated,"
+                      " use path (first positional) instead",
+                      DeprecationWarning)
+        path = pwd
+    # Catch callers that only use ketword arguments but fail to pass
+    # either ``pwd`` or ``path``.
+    if path is None:
+        raise TypeError("The path argument is required")
     if context is None:
         context = "svirt_sandbox_file_t"
     if recursive:
@@ -43,8 +56,9 @@ def get_selinux_context(path):
     """
     When selinux is enabled, return the context of ``path``
     :param path: Full or relative path to a file or directory
-    :return: SELinux context as a string or None if not set
-    :raises IOError: When file does not exist.
+    :return: SELinux context as a string
+    :raises IOError: As per usual.  Documented here as it's
+    a behavior difference from ``set_selinux_context()``.
     """
     # First list item is null-terminated string length
     return selinux.getfilecon(path)[1]
